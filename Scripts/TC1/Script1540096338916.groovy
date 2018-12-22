ad5372ad57347b396b5544ba27e073a11009cd05
kazurayam <kazuaki.urayama@gmail.com>
2018-12-22 14:44:00 +0900

    print the Test Object in JSON

--- Scripts/TC1/Script1540096338916.groovy
+++ Scripts/TC1/Script1540096338916.groovy
@@ -1,11 +1,19 @@
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 
+import com.kms.katalon.core.testobject.TestObject
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
 
 WebUI.openBrowser('')
 
 WebUI.navigateToUrl('http://www.angularjshub.com/examples/forms/textinputs/')
 
-WebUI.verifyElementPresent(findTestObject('Object Repository/Page_AngularJS Hub  Text Inputs/iframe_Example_exampleIFrame'), 10)
+TestObject to = findTestObject('Object Repository/Page_AngularJS Hub  Text Inputs/iframe_Example_exampleIFrame')
+
+ObjectMapper mapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT)
+println mapper.writeValueAsString(to)
+
+WebUI.verifyElementPresent(to, 10)
 
 WebUI.closeBrowser()
